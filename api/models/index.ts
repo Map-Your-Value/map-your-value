9cb16317843e0c5d428bff8fb8927b66dac74e49
lebraquo <lebraquohack@gmail.com>
2024-06-01 23:58:21 +0200

    change the back lib in the front

--- api/models/index.ts
+++ api/models/index.ts
@@ -1,10 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './BodyFilterSearchByCompetitorFilterCompetitorPost';
-export * from './DescriptionOfACompany';
-export * from './Feature';
 export * from './HTTPValidationError';
-export * from './RankCompetitorList';
-export * from './RankFeatureList';
 export * from './ValidationError';
 export * from './ValidationErrorLocInner';
